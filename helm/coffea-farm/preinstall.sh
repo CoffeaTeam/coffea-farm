baaa9ceaaa0e4ca7abc17a71550d98ea1cbccbd2
Nick Smith <nick.smith@cern.ch>
2020-08-05 20:46:33 -0500

    TLS in dask

--- helm/coffea-farm/preinstall.sh
+++ helm/coffea-farm/preinstall.sh
@@ -8,3 +8,9 @@ openssl rsa -in ~/.globus/userkey.pem | kubectl create secret generic grid-cert 
 kubectl create secret generic grid-proxy \
   --from-file=x509up=$(voms-proxy-info --path) \
   --dry-run -o yaml --save-config | kubectl apply -f -
+
+kubectl create secret generic dask-tls-certs \
+  --from-file=ca.crt \
+  --from-file=ca.key \
+  --from-file=hostcert.pem \
+  --dry-run -o yaml --save-config | kubectl apply -f -
